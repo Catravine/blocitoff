21bc30c3f4995560edc9c2485dcb6cf6ce369bf5
Caroline Courtney <caroline@carolinecourtney.com>
2016-02-26 16:10:38 -0500

    Production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,9 +7,6 @@ Rails.application.configure do
   #config.action_mailer.raise_delivery_errors = true
   #config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    :address => "127.0.0.1",
-    :port    => 25,
-    :domain  => 'http://vast-sands-93293.herokuapp.com/'
     :address => "smtp.gmail.com",
     :port => 587,
     :authentication => :plain,
