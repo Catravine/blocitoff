e3fe13eb0a2993bd0c8f3e20ea85ddd0b79c3661
Caroline Courtney <caroline@carolinecourtney.com>
2016-02-29 17:22:58 -0500

    Seeded Data with Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Create Users
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: "helloworld",
+    password_confirmation: "helloworld",
+    confirmed_at: Date.today
+  )
+end
+
+# Create a user for my primary email
+User.create!(
+  email: "caroline@carolinecourtney.com",
+  password: "helloworld",
+  password_confirmation: "helloworld",
+  confirmed_at: Date.today
+)
+
+# Create a user for my secondary email
+User.create!(
+  email: "catravine@hotmail.com",
+  password: "helloworld",
+  password_confirmation: "helloworld",
+  confirmed_at: Date.today
+)
+
+# Total Users
+users = User.all
+
+# Create To-Do items
+50.times do
+  Item.create(name: Faker::Hacker.say_something_smart, user: users.sample)
+end
+
+# Results
+puts "Seed Finished..."
+puts "#{User.count} users created"
+puts "#{Item.count} items created"
