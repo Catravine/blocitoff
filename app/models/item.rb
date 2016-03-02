7831dd28cc2960b6364a42a8d68b277af58b70cd
Caroline Courtney <caroline@carolinecourtney.com>
2016-03-02 03:34:38 -0500

    Tasks about to expire are noted and at the top

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,9 @@
 class Item < ActiveRecord::Base
   belongs_to :user
 
+  default_scope { order(created_at: :asc) }
+
   def finish_or_delete_by
-    #7.days.from_now.to_date
     created_at.to_date + 7.days
   end
 
